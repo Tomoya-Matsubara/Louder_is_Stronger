c00be37ebf87f297f75be2d879922bfc45909c1e
Yuka Nokihara <nokihara-y2896@keio.jp>
2021-11-19 18:09:16 +0900

    ゲーム画面(制限時間)→結果画面(ボタン)→スタート画面の遷移

---
+++ Assets/Scenes/Button.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void TitleGameButton() {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ResultTitleButton(){
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    
+}
