a895ead6dff543cbd2b7ad37271aec0a9b105b96
LAPTOP-L2QTLTP5\mizuk <mizuki_obayashi@keio.jp>
2021-11-19 18:14:39 +0900

    PlayerとEnemyのアニメーションをつけました．

---
+++ Assets/ToonyTinyPeople/TT_demo/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    // Animator コンポーネント
+    private Animator animator;
+
+    // 設定したフラグの名前
+    private const string key_isShout = "isShout";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // WaitからShoutに切り替える処理
+        // スペースキーを押下している
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // WaitからShoutに遷移する
+            this.animator.SetBool(key_isShout, true);
+        }
+        else
+        {
+            // ShoutからWaitに遷移する
+            this.animator.SetBool(key_isShout, false);
+        }
+    }
+}
