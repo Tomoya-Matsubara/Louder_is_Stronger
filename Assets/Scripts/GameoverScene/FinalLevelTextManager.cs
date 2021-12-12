76d75aebc565db9e871c8ef92d086eaf8976825c
Tomoya Matsubara <matsubara.tomoya@student-cs.fr>
2021-12-12 14:35:32 +0900

    ResultScene, GameoverSceneのUI調整

---
+++ Assets/Scripts/GameoverScene/FinalLevelTextManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+using UnityEngine.UI;
+
+public class FinalLevelTextManager : MonoBehaviour
+{
+    private TextMeshProUGUI levelClearText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelClearText = gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        levelClearText.text = $"Your Level: {EnemyLevel.currentLevel}";
+    }
+}
