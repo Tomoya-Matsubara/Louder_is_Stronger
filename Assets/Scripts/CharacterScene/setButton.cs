89f6d5d626ab123acca3bd1f43ad396af790b27d
Yuka Nokihara <nokihara-y2896@keio.jp>
2022-01-09 18:08:32 +0900

    キャラクター選択画面完全版

---
+++ Assets/Scripts/CharacterScene/setButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class setButton : MonoBehaviour
+{
+
+    public static int charaindex = 1;
+
+    public void setid(int id) {
+        charaindex = id;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
