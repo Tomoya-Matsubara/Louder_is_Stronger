d155ae1afeed112da3c1306d04d824ecc8f06d5d
Tomoya Matsubara <matsubara.tomoya@student-cs.fr>
2022-01-16 21:28:32 +0900

    各ステージ終わりにベストスコアを表示

--- Assets/Scripts/ScoreTextManager.cs
+++ Assets/Scripts/ScoreTextManager.cs
@@ -7,24 +7,26 @@ using UnityEngine.UI;
 public class ScoreTextManager : MonoBehaviour
 {
     private TextMeshProUGUI scoreText;
+    public string mode;
+
     // Start is called before the first frame update
     void Start()
     {
         scoreText = gameObject.GetComponent<TextMeshProUGUI>();
         scoreText.text = $"Score: {ScoreManager.score}";
 
-        if (PlayerPrefs.HasKey("Best Score")) 
+        if (PlayerPrefs.HasKey(mode)) 
         {
-            int best_score = PlayerPrefs.GetInt("Best Score");
+            int best_score = PlayerPrefs.GetInt(mode);
             
             if (best_score < ScoreManager.score)
             {
-                PlayerPrefs.SetInt("Best Score", ScoreManager.score);
+                PlayerPrefs.SetInt(mode, ScoreManager.score);
             }
         }
         else
         {
-            PlayerPrefs.SetInt("Best Score", ScoreManager.score);
+            PlayerPrefs.SetInt(mode, ScoreManager.score);
         }
     }
 
